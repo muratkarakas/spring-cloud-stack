0e5903a5a9b3fc8eaf7e9ffd1a81d139e14e7f7d
mkarakas <murat@eteration.com>
2017-02-20 16:53:02 +0200

    docker files

---
+++ dockerRun.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+cd sc-config-server
+mvn package docker:build -DskipTests
+
+cd ..
+cd sc-eureka-server
+mvn  package docker:build -DskipTests
+
+cd ..
+cd sc-service
+mvn  package docker:build -DskipTests
+
+cd ..
+cd sc-service-client
+mvn  package docker:build -DskipTests
+
+
+cd ..
+cd sc-zipkin
+mvn  package docker:build -DskipTests
+
+cd ..
+docker-compose up
